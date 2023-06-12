592c796ed6307acd5dc13bfa9d872354800e28a9
rodrigusbarri <132109653+rodrigusbarri@users.noreply.github.com>
2023-06-13 00:11:01 +0200

    aplicar estilos bootstrap

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,50 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-export default function Navbar () {
+export default function Navbar() {
   return (
-    <>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
-        <li>
-          <Link href='/services'>Services</Link>
-        </li>
-      </ul>
-    </>
-);  
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" href="/">
+            Usuarios APP
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="/services">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link disabled" href="/">
+                  Contacto
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+  );
 }
--- components/Users.jsx
+++ components/Users.jsx
@@ -1,32 +1,36 @@
 "use client";
 import { useRouter } from "next/navigation";
 
-
 export default function Users({ users }) {
-
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <>
-      <ul>
-        {users.map((user) => (
-          <li
-            key={user.id}
-            onClick={() => {
-              router.push(`/users/${user.id}`);
-            }}
-          >
-            <div>
-              <h5>
-                {user.first_name}
-                {user.last_name}
-              </h5>
-              <img src={user.avatar} alt={user.email} />
-              <p>{user.email}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className="list-group">
+      {users.map((user) => (
+        <li
+          style={{ cursor: "pointer" }}
+          key={user.id}
+          className="list-group-item d-flex justify-content-between aling-tems-center list-group-item-action"
+          onClick={() => {
+            router.push(`/users/${user.id}`);
+          }}
+        >
+          <div>
+            <h5>
+              {user.first_name}
+              {user.last_name}
+            </h5>
+
+            <p>{user.email}</p>
+          </div>
+          <img 
+          src={user.avatar}
+          alt={user.email}
+          style={{width: '80px', borderRadius: '80%'
+          }}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
